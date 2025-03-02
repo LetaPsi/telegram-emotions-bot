d5f561fb1f9c053171a640456baa05f6b8111594
LetaPsi <viola.k.1994@gmail.com>
2025-03-02 13:23:15 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Токен вашего Telegram-бота
+TELEGRAM_TOKEN = '7564115533:AAHXDIcvivh26L_ylbHIdpgBb1_WnNfzrM4'
+
+# Токен OpenAI API (если используется для анализа)
+OPENAI_API_KEY = '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'
+
+WEBHOOK_URL = 'https://api.render.com/deploy/srv-cv22sblsvqrc738rlj8g?key=Zo8VAtoPjkI'
